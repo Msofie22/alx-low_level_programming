3239f4e45810ebb807769b92e7f008138f7d7da4
Msofie22 <nakawooyasophie22@gmail.com>
2023-09-11 04:21:45 -0700

    fix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,11 +9,11 @@ int main(void)
 char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 int i,
 
-for (i = 0, i < 26, i++)
+for (int i = 0, i < 26, i++)
 {
 putchar (alp(i));
 }
-putchar("\n");
+putchar('\n');
 return (0);
 }
 
