2df5761ae9ceb924e4b793788b002da9b8c3dec1
Msofie22 <nakawooyasophie22@gmail.com>
2023-09-11 05:16:17 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
 * main - entry point
 *
-*Return: Always 0
+*Return: Always (0)
 **/
 int main(void)
 {
-printf("with proper grammar, but the out come is a piece of art,\n");
+printf("but the out come is a piece of art,\n");
 return (0);
 }
