d77827e1faee1f5e1cda2ef2b2cebfaf85958e7a
Msofie22 <nakawooyasophie22@gmail.com>
2023-09-14 07:08:23 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*mul - multiples two intergers
+*@a: - The first interger
+*@b: - The second interger
+*Return: Always (0)
+**/
+
+int mul(int a, int b)
+{
+int result = a * b;
+return (result);
+}
