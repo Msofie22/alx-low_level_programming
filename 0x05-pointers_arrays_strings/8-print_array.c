0daee717b0190dceaee5771dfd2676b504b7d20b
Msofie22 <nakawooyasophie22@gmail.com>
2023-09-18 15:23:03 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,10 +12,11 @@ int i;
 for (i = 0; i < n; i++)
 {
 printf("%d ", a[i]);
-if (i != (n - 1))
+}
+if (i == (n - 1))
 {
-printf("%d", a(n - 1));
+printf("%d", a[n-1]);
 }
-}
-putchar('\n');
+{
+printf("\n");
 }
