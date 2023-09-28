2150439c8908845ed4280b8e203f3fbaf3ea7ade
Msofie22 <nakawooyasophie22@gmail.com>
2023-09-28 12:25:59 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -29,9 +29,5 @@ if (i * i == n)
 return (i);
 return (actual_sqrt_recursion(n, i + 1));
 }
-if (i * i > n)
-return (-1);
-if (i * i == n)
-return (i);
-return (actual_sqrt_recursion(n,  i + 1));
+
 }
