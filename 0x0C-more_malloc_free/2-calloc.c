ec89e50d249a3bc163058ab9ff7711b66a27a57a
Msofie22 <nakawooyasophie22@gmail.com>
2023-10-08 12:50:09 -0700

    files

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,7 +8,7 @@
 *Return: pointer to the memory area s
 **/
 
-char *_memset(char *s, char b, unsinged int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
 for (i = 0; i < n; i++)
