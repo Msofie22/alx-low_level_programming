cdcf71a461bfd3a83b7724b4aa3b528823903c17
Msofie22 <nakawooyasophie22@gmail.com>
2023-10-26 05:34:25 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,7 +3,7 @@
 /**
 *print_binary - prints the binary equivalent of a decimal number
 *@n: number to print in binary
-*Description:print_binary
+*
 **/
 void print_binary(unsigned long int n)
 {
@@ -18,7 +18,7 @@ _putchar('1');
 count++;
 }
 else if (count)
-_putcahr('0')
+_putchar('0')
 }
 if (!count)
 _putchar('0');
