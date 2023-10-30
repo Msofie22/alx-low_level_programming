08a9955f4a19b996ab41237879995c3eca2e4839
Msofie22 <nakawooyasophie22@gmail.com>
2023-10-30 14:54:54 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /**
 *main - copies content of a file to another
@@ -23,7 +26,7 @@ if (fd_from == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-fd_to = open(argv[2], O_GREAT | O_WRONLY | O_TRUNC, 0664);
+fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
