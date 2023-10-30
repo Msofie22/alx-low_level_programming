9c93690ef0131881a5b753e832c4092c67c0fb51
Msofie22 <nakawooyasophie22@gmail.com>
2023-10-30 13:52:39 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,16 +11,23 @@ int create_file(const char *filename, char *text_content)
 {
 int fd, len, x;
 if (!filename)
-return (-1)
+return (-1);
 fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+if (fd == -1)
 return (-1);
 if (!text_content)
+{
+close(fd);
 return (1);
+}
 for (len = 0; text_content[len] != '\0'; len++)
 ;
 x = write(fd, text_content, len);
 if (x == -1)
+{
+close(fd);
 return (-1);
+}
 close(fd);
 return (1);
 }
