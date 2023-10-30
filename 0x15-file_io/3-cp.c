6bf06279169ca8f57556c1c52b3c5d74d7746aea
Msofie22 <nakawooyasophie22@gmail.com>
2023-10-30 14:46:57 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-int fd_from, fd_to, r = 1, w, cf, ct;
+int fd_from, fd_to, r = 1, w;
 char buf[1024];
 if (argc != 3)
 {
@@ -26,7 +26,7 @@ exit(98);
 fd_to = open(argv[2], O_GREAT | O_WRONLY | O_TRUNC, 0664);
 if (fd_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n",argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 while (r > 0)
@@ -37,24 +37,20 @@ if (r == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-if (r > 0)
-{
 w = write(fd_to, buf, r);
 if (w == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n",argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
 }
-cf = close(fd_from);
-if (cf == -1)
+if (closefd_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from),
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 exit(100);
 }
-ct = close(fd_to);
-if (ct == -1)
+if (close(fd_to == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 exit(100);
